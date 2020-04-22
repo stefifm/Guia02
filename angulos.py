b6465489cee12cbebc12a9462a4bc8869617c3b3
Stefania <bruerastefania@gmail.com>
2020-04-22 15:26:05 -0300

    angulos.py

---
+++ angulos.py
@@ -0,0 +1,13 @@
+____author___="Cátedra AED"
+
+print("Cálculo de ángulos")
+
+#datos
+x=int(input("Cargar valor de x: "))
+y=int(input("Cargar valor de y: "))
+#procesos
+alfa=x-(y-x)/-2
+beta=(y-x)/-2
+#resultados
+print("El valor de alfa es: ", alfa)
+print("El valor de beta es: ", beta)
